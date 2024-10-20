8fec850f836d856921d1cd82081b9174b37c0644
riaz-07 <riazcodjo@gmail.com>
2024-10-20 23:23:19 +0100

    Update ai1.js

--- scripts/cmds/ai1.js
+++ scripts/cmds/ai1.js
@@ -1,63 +1,66 @@
-const axios = require('axios');
-
-const ArYAN = [
-  'ai'
-];
-
-module.exports = {
-  config: {
-    name: 'ai',
-    version: '1.0.1',
-    author: 'OtinxSandip | ArYAN',
-    role: 0,
-    category: 'ai',
-    longDescription: {
-      en: 'Ai is a large language model trained by OpenAi.',
-    },
-    guide: {
-      en: 'Ai <questions>\n\n🔎 𝗚𝘂𝗶𝗱𝗲\nAi who are you?\nAi what is Love?',
-    },
-  },
-
-  langs: {
-    en: {
-      final: "",
-      loading: 'Answering your question, 𝗉𝗅𝖾𝖺𝗌𝖾 𝗐𝖺𝗂𝗍........'
-    }
-  },
-
-  onStart: async function () {},
-  onChat: async function ({ api, event, args, getLang, message }) {
-    try {
-      const prefix = ArYAN.find((p) => event.body && event.body.toLowerCase().startsWith(p));
-
-      if (!prefix) {
-        return;
-      }
-
-      const prompt = event.body.substring(prefix.length).trim();
-
-      const loadingMessage = getLang("loading");
-      const loadingReply = await message.reply(loadingMessage);
-      
-      const response = await axios.get(`https://itsaryan.onrender.com/api/gpt?prompt=${encodeURIComponent(prompt)}&model=gpt-4`);
-
-      if (response.status !== 200 || !response.data || !response.data.original) {
-        throw new Error('Invalid or missing response from API');
-      }
-
-      const messageText = response.data.original; 
-
-      const finalMsg = `${messageText}`;
-      api.editMessage(finalMsg, loadingReply.messageID);
-
-      console.log('Sent answer as a reply to user');
-    } catch (error) {
-      console.error(`Failed to get answer: ${error.message}`);
-      api.sendMessage(
-        `${error.message}.`,
-        event.threadID
-      );
-    }
+const { getPrefix, getStreamFromURL, uploadImgbb } = global.utils;
+async function ai({ message: m, event: e, args: a, usersData: u }) {
+  var p = [`${await getPrefix(e.threadID)}${this.config.name}`,
+`${this.config.name}`
+/*"ai"
+*you can add more prefix here
+*/
+]; 
+ if (p.some(b => a[0].toLowerCase().startsWith(b))) {
+try {      
+let prompt = "";
+if (e.type === "message_reply" && e.messageReply.attachments && e.messageReply.attachments[0]?.type === "photo") {
+ const b = await uploadImgbb(e.messageReply.attachments[0].url);
+prompt = a.slice(1).join(" ") + ' ' + b.image.url;
+} else {
+ prompt = a.slice(1).join(" ");
+}
+ var __ = [{ id: e.senderID, tag: await u.getName(e.senderID) }];
+ const r = await require("axios").post(`https://test-ai-ihc6.onrender.com/api`, {
+  prompt: prompt,
+ apikey: "GayKey-oWHmMb1t8ASljhpgSSUI",
+  name: __[0]['tag'],
+ id: __[0]['id'],
+ });
+var _ = r.data.result.replace(/{name}/g, __[0]['tag']).replace(/{pn}/g, p[0]);
+ if (r.data.av) {
+ if (Array.isArray(r.data.av)) {
+ const avs = r.data.av.map(url => getStreamFromURL(url));
+ const avss = await Promise.all(avs);
+  m.reply({
+ body: _,
+ mentions: __,
+ attachment: avss
+ });
+ } else {
+ m.reply({
+ body: _,
+ mentions: __,
+attachment: await getStreamFromURL(r.data.av)
+  });
   }
+  } else {
+m.reply({
+body: _,
+mentions: __
+  });
+  }
+  } catch (error) {
+ m.reply("Error " + error);
+ }
+ }
+}
+module.exports = {
+config: {
+ name: "ai",
+aliases: [],
+version: 1.6,
+author: "Jun",
+role: 0,
+ shortDescription: "An AI that can do various tasks",
+ guide: "{pn} <query>",
+ category: "AI"
+ },
+ onStart: function() {},
+ onChat: ai
 };
